de46c53ff68724005f5c5866fe989d5ef957b183
md <mdmostofakhan1972@gmail.com>
2023-04-14 00:00:23 +0600

    frome cart saved

--- src/component/Order/Order.jsx
+++ src/component/Order/Order.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import './Order.css'
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
-    const cart = useLoaderData()
-    console.log(cart)
+    const saveCart = useLoaderData()
+    const [cart, setCart] = useState(saveCart);
+ 
+
+    const handleRemoveFromCart = (id) =>{
+        const remening = cart.filter(product => product.id !== id);
+        setCart(remening)
+        removeFromDb(id)
+    }
+
     return (
         <div className='shop-container'>
            <div className='review-container'>
                {
-                    cart.map(product => <ReviewItem
+                cart.map(product => <ReviewItem
                     key={product.id}
                     product={product}
+                    handleRemoveFromCart ={handleRemoveFromCart}
                     ></ReviewItem>)
                }
            </div>
--- src/component/ReviewItem/ReviewItem.jsx
+++ src/component/ReviewItem/ReviewItem.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faTrashAlt} from '@fortawesome/free-solid-svg-icons'
 import './RevieeItem.css'
 
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product, handleRemoveFromCart}) => {
   const {id, img, price, name, quantity} = product;
     return (
-        <div className='review-item'>
-            <img src={img} alt="" />
-         <div  className='review-details'>
-           <h4 className='product-title'>{name}</h4>
-            <p>price: <span className='orenge-text'>${price}</span> </p>
-            <p>order Quantity <span className='orenge-text'>${quantity}</span> </p>
-
-         </div>
-        </div>
+          <div className='review-item'>
+              <img src={img} alt="" />
+          <div  className='review-details'>
+            <h4 className='product-title'>{name}</h4>
+              <p>price: <span className='orenge-text'>${price}</span> </p>
+              <p>order Quantity <span className='orenge-text'>${quantity}</span> </p>
+          </div>
+          <button onClick={() => handleRemoveFromCart(id)}  className='btn-delete'>
+          <FontAwesomeIcon className='delet-icon' icon={faTrashAlt} />
+          </button>
+          </div>
     );
 };
 
